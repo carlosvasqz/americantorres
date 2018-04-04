3e2158cbe25d9dcd5eb2cc5be76a7bf2e55e9e67
Kunai11 <carlomeza11.cm@gmail.com>
2018-04-04 17:25:04 -0600

    Cambios

--- source/menu.php
+++ source/menu.php
@@ -28,7 +28,7 @@ function menuGeneral() {
           <li class="treeview"><a href="#"><i class="fa fa-laptop"></i><span>Control de Inventario</span><i class="fa fa-angle-right"></i></a>
             <ul class="treeview-menu">
               <li><a href="ci_consultar_articulo.php"><i class="fa fa-circle-o"></i>Consultar Articulo</a></li>
-              <li><a href="ci_modificar_articulo.php"><i class="fa fa-circle-o"></i>Modificar Articulo</a></li>
+              <li><a href="ci_modificar_articulo.php?codigo_buscar=null"><i class="fa fa-circle-o"></i>Modificar Articulo</a></li>
               <li><a href="ci_catalogo.php"><i class="fa fa-circle-o"></i>Catalago de Articulos</a></li>
             </ul>
           </li>
