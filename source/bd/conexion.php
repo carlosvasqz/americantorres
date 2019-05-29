acec6f2d0ebed189cf40c682dc336f388bbc882d
carlosvasqz <carlosdavidvasquez7@gmail.com>
2019-05-29 09:47:56 -0600

    Archivo de conexion

---
+++ source/bd/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$hostname_cn = "localhost";
+$database_cn = "americantorres";
+$username_cn = "root";
+$password_cn = "";
+$db = mysqli_connect($hostname_cn,$username_cn,$password_cn,$database_cn);
+?>
+ 
